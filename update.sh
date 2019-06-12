2106f9f0f67aa936ee4e208b472479a961164c65
Tom Hauschke <tom.hauschke@web.de>
2019-06-12 17:56:55 +0200

    fix update script

--- update.sh
+++ update.sh
@@ -4,10 +4,7 @@
 set -e
 
 # check and install required packages
-if [ ! pacman -Qi git > /dev/null ] || [ ! pacman -Qi ansible > /dev/null ];
-then
-    pacman -Syyuq --noconfirm git ansible
-fi
+pacman -Syyuq --noconfirm git ansible
 
 # run ansible
 ansible-pull -U https://github.com/H3xaT0m/ansible-workstation.git main.yml
